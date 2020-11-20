8b771c893819e48ea93bb6beaf0a7c40507f4e46
jilliankmartin <jillmartin@live.com>
2020-11-20 08:59:41 -0800

    Added hooks files

---
+++ client/src/hooks/useArena.js
@@ -0,0 +1,2 @@
+// makes a get request to the API for arena information
+// controls the state of the arena - which theme is the player in - background image, music etc
---
+++ client/src/hooks/usePlayerAction.js
@@ -0,0 +1,2 @@
+// controls the player's state - health, words etc
+// Makes an Axios request to words to load the player's words
