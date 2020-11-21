dd3ee588d132ff3a7509b18304503f962453d63c
Sherwin <sherwin.kwan92@gmail.com>
2020-11-20 18:26:29 -0800

    trying to fix bug

--- client/src/components/HealthBar.js
+++ client/src/components/HealthBar.js
@@ -6,7 +6,7 @@ import './HealthBar.scss';
 function HealthBar(props) {
   return (
     <div className="health-bar">
-      <img src="/images/red-cross.png" alt="Health" onClick={props.onClick}/>
+      <img src="/images/red-cross.png" alt="Health" onClick={() => props.onClick(10)}/>
       <SkillBar animationDelay={0} animationDuration={200} height={40} skills={[{type: 'HEALTH', level: props.initialHealth}]} />
     </div>
   );
