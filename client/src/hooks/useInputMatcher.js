6a56b34e1bd52ddd40fb82865490063715001eee
helenohyeah <ouyang.helen@gmail.com>
2020-11-22 14:49:26 -0500

    Add handling for blank input

--- client/src/hooks/useInputMatcher.js
+++ client/src/hooks/useInputMatcher.js
@@ -1,16 +1,15 @@
-import { useState } from "react";
-
 export default function useInputMatcher() {
 
-  // const [ isLetterMatch, setIsLetterMatch ] = useState(false);
-  // const [ isWordMatch, setIsWordMatch ] = useState(false);
-
-  // Check for letter matches and returns true if a match is found
   // playerInput: "word"
   // playerActions: [{ name: "attack", word: "word"}, { name: "heal", word: "girl" }]
+
+  // Check for letter matches and returns true if a match is found
   const handleLetterMatch = (playerInput, playerActions) => {
-    const actionWordSlices = playerActions.map(action => action.word.slice(0, playerInput.length) || '');
-    return (actionWordSlices.includes(playerInput)) ? true : false;
+    if (playerInput.length > 0 && playerActions.length > 0) {
+      const actionWordSlices = playerActions.map(action => action.word.slice(0, playerInput.length));
+      return (actionWordSlices.includes(playerInput)) ? true : false;
+    }
+    return false;
   }
 
   // Check for word matches and returns the action name if a match is found
