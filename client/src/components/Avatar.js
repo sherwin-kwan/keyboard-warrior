5e8e571afd397af51497a60ff5a89c20321f2c70
helenohyeah <ouyang.helen@gmail.com>
2020-11-23 11:36:50 -0500

    Style health bar

--- client/src/components/Avatar.js
+++ client/src/components/Avatar.js
@@ -4,8 +4,8 @@ import React from "react";
 function Avatar(props) {
   return (
     <>
+      <h2>{props.name}</h2>
       <img src={props.filename} width={props.width} height={props.height} />
-      <p>{props.name}</p>
     </>
   );
 }
--- client/src/components/HealthBar.js
+++ client/src/components/HealthBar.js
@@ -4,10 +4,18 @@ import SkillBar from 'react-skillbars';
 import './HealthBar.scss';
 
 function HealthBar(props) {
+  const colors = {
+    "bar": "#00E436",
+    "title": {
+      "text": "#000",
+      "background": "#fff"
+    }
+  };
+
   return (
     <div className="health-bar">
-      <img src="/images/red-cross.png" alt="Health" onClick={() => props.onClick(10)}/>
-      <SkillBar animationDelay={0} animationDuration={200} height={40} skills={[{type: 'HEALTH', level: props.health}]} />
+      <img src="/images/red-cross.png" alt="Health" onClick={() => props.onClick(10)} />
+      <SkillBar animationDelay={0} animationDuration={200} height={'2rem'} skills={[{type: 'Health', level: props.health}]} colors={colors}/>
     </div>
   );
 }
