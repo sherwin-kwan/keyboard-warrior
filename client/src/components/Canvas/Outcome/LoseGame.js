53e625de025efccb2dcff6a087c69f88a2ad1480
Sherwin <sherwin.kwan92@gmail.com>
2020-11-22 22:50:34 -0800

    add win and lose screen backgrounds

--- client/src/components/Canvas/Outcome/LoseGame.js
+++ client/src/components/Canvas/Outcome/LoseGame.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 const LoseGame = (props) => {
   return (
-    <>
-      <h1>DEFEAT</h1>
-      <p>Too bad, best of luck next time!</p>
-      <button onClick={() => props.setMode('START')}>Back to Start</button>
-    </>
+    <main className="outcome lose-game">
+      <p className="message">
+        <h1>DEFEAT</h1>
+        <p>There's no sugercoating this - losing sucks. But the great thing is this is a game, so you can try again!</p>
+        <button onClick={() => props.setMode('START')}>Try Again</button>
+      </p>
+    </main>
   );
 };
 export default LoseGame;
