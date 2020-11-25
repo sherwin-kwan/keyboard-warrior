19b7181976924916920bc47bc5d4b9b5decca899
Sherwin <sherwin.kwan92@gmail.com>
2020-11-25 11:31:23 -0800

    add useGame and useBattle hooks, add api route to display words/actions in arena

--- client/src/components/Canvas/StartGame.js
+++ client/src/components/Canvas/StartGame.js
@@ -9,8 +9,26 @@ function StartGame(props) {
     <main className="start-game">
       <h1>A Typing Game</h1>
       <div className="menu">
+        <form onSubmit={event => event.preventDefault()}>
+          <label for="name">Player Name:</label><br/>
+          <input 
+            type="text" 
+            id="name" 
+            placeholder="Enter player name" 
+            name="name"
+            onChange={(event) => {
+              props.setGame(
+                {
+                  player_name: event.target.value
+                }
+              )
+            }}
+            /><br/>
+        </form>
         <button
-          onClick={() => { props.setMode("MAP") }}
+          onClick={() => { 
+            props.setMode("MAP") 
+          }}
         >Start Game
         </button>
         (Following buttons don't do anything yet:)
