85b729c7473b1b5995ce0ab44dec2b68da93dbb4
Sherwin <sherwin.kwan92@gmail.com>
2020-11-26 15:56:48 -0800

    fix door arrows

--- client/src/components/ArenaDoor.js
+++ client/src/components/ArenaDoor.js
@@ -2,54 +2,69 @@
 import React from "react";
 
 function ArenaDoor(props) {
-  // console.log('Props.arena is: ', props.arena);
+
+  const { arenas, doorTagArray, currentDoor, setCurrentDoor } = props;
+
+  console.log('Arenas is: ', arenas);
+  const arena = arenas[currentDoor];
+  console.log('Arena is: ', arena);
+  console.log('Current index is: ', currentDoor);
+
   const pointsIcon = '/images/green-diamond.png';
-  const disabled = (typeof props.arena.beaten === 'boolean'); // Either "true" or "false" showing that the player has already either won or lost
+  const disabled = (typeof arena.beaten === 'boolean'); // Either "true" or "false" showing that the player has already either won or lost
   // in this arena, will cause the arena door to be disabled so player cannot enter it again
-  console.log(`disabled for ${props.arena.name} is: `, disabled);
   const doorClassName = disabled ? "card card-disabled" : "card";
 
+  const setPreviousDoor = () => {
+    console.log('Changing to previous door');
+    setCurrentDoor(prev => (prev !== 0) ? prev - 1 : prev);
+  };
+
+  const setNextDoor = () => {
+    setCurrentDoor(prev => (prev !== arenas.length - 1) ? prev + 1 : prev);
+  };
+
   return (
-    <div className="door" id={props.arena.name.toLowerCase().replace(/\s/g, '-')}>
-      <img className="door-arrow" src="/images/left_arrow_circle.png"/>
-      <div 
+    <div className="door">
+      <img className="door-arrow" src="/images/left_arrow_circle.png" onClick={setPreviousDoor} />
+      <div
         className={doorClassName}
         title={disabled ? `No rematches! Please choose another arena` : `Enter arena`}
         onClick={() => {
           if (!disabled) {
             props.setGameMode("ARENA");
-            props.setArena(props.arena);
+            props.setArena(arena);
           }
         }}>
-        <h3>{props.arena.name}</h3>
-        <img 
+        <h3>{arena.name}</h3>
+        <img
           className="door-image"
-          src={props.arena.arena_card} 
-          alt={props.arena.name}
+          src={arena.arena_card}
+          alt={arena.name}
         />
         <footer>
           {!props.beaten &&
-          <>
-            <div className="difficulty">
-              <img
-                src={props.arena.Difficulty.icon} 
-                alt={props.arena.Difficulty.name}
-              />
-              {props.arena.Difficulty.name}
-            </div>
-            <div className="points">
-              <img
-                src={pointsIcon} 
-                alt={props.arena.points + " points"}
-              />
-              {props.arena.points + " PTS"}
-            </div>
-          </>}
-          {(props.arena.beaten === true) && <aside>Congrats, you beat this level!</aside>} 
-          {(props.arena.beaten === false) && <aside>You were defeated!</aside>} 
+            <>
+              <div className="difficulty">
+                <img
+                  src={arena.Difficulty.icon}
+                  alt={arena.Difficulty.name}
+                />
+                {arena.Difficulty.name}
+              </div>
+              <div className="points">
+                <img
+                  src={pointsIcon}
+                  alt={arena.points + " points"}
+                />
+                {arena.points + " PTS"}
+              </div>
+            </>}
+          {(arena.beaten === true) && <aside>Congrats, you beat this level!</aside>}
+          {(arena.beaten === false) && <aside>You were defeated!</aside>}
         </footer>
       </div>
-      <img className="door-arrow" src="/images/right_arrow_circle.png"/>
+      <img className="door-arrow" src="/images/right_arrow_circle.png" onClick={setNextDoor} />
     </div>
   );
 }
