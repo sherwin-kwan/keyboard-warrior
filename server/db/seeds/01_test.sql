92c1e7b7e970f2ae81e94f5a0119d929228791c5
Sherwin <sherwin.kwan92@gmail.com>
2020-11-25 20:00:29 -0800

    remove unneeded files on sql

--- server/db/seeds/01_test.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO users (name, email, password) VALUES ('Alice', 'a@a.ca', '1');
-INSERT INTO users (name, email, password) VALUES ('Kira', 'a@b.ca', '1');
-INSERT INTO users (name, email, password) VALUES ('Heather', 'a@c.ca', '1');
-INSERT INTO users (name, email, password) VALUES ('Logan', 'a@d.ca', '1');
