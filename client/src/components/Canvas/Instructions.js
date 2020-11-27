eb5889126fe9a0911acee8fb71f561544bd13f4e
helenohyeah <ouyang.helen@gmail.com>
2020-11-26 21:41:33 -0500

    Add instruction pictures and style some more

--- client/src/components/Canvas/Instructions.js
+++ client/src/components/Canvas/Instructions.js
@@ -8,15 +8,15 @@ function Instructions(props) {
         <h1>Instructions</h1>
         <h2>Your Objective:</h2>
         <p>Battle your way through 5 arenas to get to the boss. Beat the boss and win the game!</p>
+        <img src="/images/progress-bar.png" alt="Progress bar" />
         <h2>Battling:</h2>
-        <p>Type the words that show up to attack or heal.</p>
-        <img src="" alt="Attack icon" />Deal 10 damage
-        <img src="" alt="Heal icon" />Heal 10 health
+        <p>Type the words that show up to attack for 10 damage or heal 10 health.</p>
+        <img src="/images/words-to-type.png" alt="Battle arena" />
         <h2>Scoring:</h2>
-        <p>Earn more points by choosing more difficult arenas.
-        <img src="" alt="Points" />
-        Beat the game faster to get a bonus multiplier.
-        <img src="" alt="Leaderboards" /></p>
+        <p>Earn more points by choosing more difficult arenas.</p>
+        <img src="/images/difficulty-and-points.png" alt="Difficulty and points" />
+        <p>Beat the game faster to get a bonus multiplier and top the leaderboards!</p>
+        <img src="/images/leaderboards.png" alt="Leaderboards" />
         <button
           className="primary"
           onClick={() => props.setMode("START")}
