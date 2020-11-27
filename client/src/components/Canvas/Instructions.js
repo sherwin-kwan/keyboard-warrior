18db10248a93a860b81dcdcada2938de9e863439
helenohyeah <ouyang.helen@gmail.com>
2020-11-26 21:25:36 -0500

    Add contents and initial styling

--- client/src/components/Canvas/Instructions.js
+++ client/src/components/Canvas/Instructions.js
@@ -1,15 +1,29 @@
 import React from 'react';
+import './Instructions.scss'
 
 function Instructions(props) {
   return (
-    <main className="Instructions">
-      <h1>Instructions</h1>
-      <button
-        className="primary"
-        onClick={() => props.setMode("START")}
-      >
-        Back
-      </button>
+    <main className="instructions">
+      <div className="instructions">
+        <h1>Instructions</h1>
+        <h2>Your Objective:</h2>
+        <p>Battle your way through 5 arenas to get to the boss. Beat the boss and win the game!</p>
+        <h2>Battling:</h2>
+        <p>Type the words that show up to attack or heal.</p>
+        <img src="" alt="Attack icon" />Deal 10 damage
+        <img src="" alt="Heal icon" />Heal 10 health
+        <h2>Scoring:</h2>
+        <p>Earn more points by choosing more difficult arenas.
+        <img src="" alt="Points" />
+        Beat the game faster to get a bonus multiplier.
+        <img src="" alt="Leaderboards" /></p>
+        <button
+          className="primary"
+          onClick={() => props.setMode("START")}
+        >
+          Got it, Let's Play!
+        </button>
+      </div>
     </main>
   );
 }
