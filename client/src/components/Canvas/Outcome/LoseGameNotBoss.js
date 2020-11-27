c62eddf39b46b8a0ad5cd9d2cdc9a7da009649a6
jilliankmartin <jillmartin@live.com>
2020-11-26 16:48:19 -0800

    fixed styling on winallarenas

--- client/src/components/Canvas/Outcome/LoseGameNotBoss.js
+++ client/src/components/Canvas/Outcome/LoseGameNotBoss.js
@@ -13,7 +13,7 @@ const LoseGameNotBoss = (props) => {
     <main className="outcome lose-game">
       <p className="message">
         <h1>DEFEAT</h1>
-        <p>Woah, you are terrible at this! You lost so many arenas there's no way you can make it to verse the boss now. You can try again from the beginning though.</p>
+        <p>Uh oh! You lost so many arenas there's no way you can make it to verse the boss now. You can try again from the beginning though.</p>
         <button className="primary" onClick={() => {
           props.resetGame()
           }
