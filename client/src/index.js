80def0bdd84e381f936266399f4b44edcf9b5a05
helenohyeah <ouyang.helen@gmail.com>
2020-11-28 17:35:21 -0500

    Add app level axios configuration

--- client/src/index.js
+++ client/src/index.js
@@ -3,9 +3,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Application from './components/Application';
 import reportWebVitals from './reportWebVitals';
+import axios from 'axios';
 
 // Styles
 import './styles/index.scss';
+
+// Axios configuration
+axios.defaults.baseURL = 'https://keyboard-warrior-api.herokuapp.com/';
+// axios.defaults.baseURL = 'http://localhost:3001';
+axios.interceptors.request.use(req => {
+  console.log('Request:', req);
+  return req;
+}, err => {
+  console.log('Request Error:', err);
+  return Promise.reject(err);
+});
+axios.interceptors.response.use(res => {
+  console.log('Response:', res);
+  return res;
+}, err => {
+  console.log('Response Error:', err);
+  return Promise.reject(err);
+});
 
 ReactDOM.render(
   <React.StrictMode>
