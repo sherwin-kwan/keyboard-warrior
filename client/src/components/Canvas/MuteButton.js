e6af91d6ae001ed39aea6759da4f5757a31b65c5
jilliankmartin <jillmartin@live.com>
2020-11-27 18:24:14 -0800

    remove log

--- client/src/components/Canvas/MuteButton.js
+++ client/src/components/Canvas/MuteButton.js
@@ -2,8 +2,6 @@ import React from 'react';
 
 const MuteButton = (props) => {
 
-  console.log(typeof props.music)
-
   return (
     <aside className="mute-music" >
       {props.music === "OFF" && <img src="/images/mute-music.png" onClick={() => {props.setMusic("ON")}} alt="unmute music"/>}
