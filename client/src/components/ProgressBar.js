3ec726e9b6ee77d206bc1c5712529fa79d9a84d0
Sherwin <sherwin.kwan92@gmail.com>
2020-11-30 14:43:31 -0800

    progress bar

--- client/src/components/ProgressBar.js
+++ client/src/components/ProgressBar.js
@@ -7,7 +7,6 @@ function ProgressBar(props) {
   return (
     <>
       <h1>Progress Bar</h1>
-      {/* Just a placeholder for now, the real progress bar will have dots or notches and look more fancy! */}
       <div className="progress-bar">
         <Avatar
         height='50px'
