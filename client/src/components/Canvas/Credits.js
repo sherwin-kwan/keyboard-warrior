b4196dea98eaa86bf0458c0e5f5c373cbc3bf688
jilliankmartin <57693368+jilliankmartin@users.noreply.github.com>
2020-11-30 15:16:54 -0800

    Update Credits.js
    Updated swapped alts and titles on credits

--- client/src/components/Canvas/Credits.js
+++ client/src/components/Canvas/Credits.js
@@ -14,7 +14,7 @@ function Credits(props) {
             <h4>Helen Ouyang</h4>
             <div className="logos">
               <a href="https://github.com/helenohyeah" rel="noopener noreferrer" target="_blank">
-                <img className="logo" alt="Github" title="LinkedIn" src="/images/github-logo.png" />
+                <img className="logo" alt="Github" title="Github" src="/images/github-logo.png" />
               </a>
               <a href="https://www.linkedin.com/in/helenouyang/" rel="noopener noreferrer" target="_blank">
                 <img className="logo" alt="LinkedIn" title="LinkedIn" src="/images/linkedin-logo.png" />
@@ -27,10 +27,10 @@ function Credits(props) {
             <h4>Jillian Martin</h4>
             <div className="logos">
               <a href="https://github.com/jilliankmartin" rel="noopener noreferrer" target="_blank">
-                <img className="logo" alt="Github" title="LinkedIn" src="/images/github-logo.png" />
+                <img className="logo" alt="Github" title="Github" src="/images/github-logo.png" />
               </a>
               <a href="https://www.linkedin.com/in/jilliankmartin/" rel="noopener noreferrer" target="_blank">
-                <img className="logo" alt="Github" title="LinkedIn" src="/images/linkedin-logo.png" />
+                <img className="logo" alt="Linkedin" title="LinkedIn" src="/images/linkedin-logo.png" />
               </a>
             </div>
           </div>
@@ -38,10 +38,10 @@ function Credits(props) {
             <h4>Sherwin Kwan</h4>
             <div className="logos">
               <a href="https://github.com/sherwin-kwan" rel="noopener noreferrer" target="_blank">
-                <img className="logo" alt="Github" title="LinkedIn" src="/images/github-logo.png" />
+                <img className="logo" alt="Github" title="Github" src="/images/github-logo.png" />
               </a>
               <a href="https://www.linkedin.com/in/sherwinkwan/" rel="noopener noreferrer" target="_blank">
-                <img className="logo" alt="Github" title="LinkedIn" src="/images/linkedin-logo.png" />
+                <img className="logo" alt="Linkedin" title="LinkedIn" src="/images/linkedin-logo.png" />
               </a>
             </div>
             <img className="portrait" src="/images/sherwin.png" alt="Sherwin" />
