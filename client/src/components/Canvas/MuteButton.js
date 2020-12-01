642cba9accbd2b10a7f8f74ccd1e401d7f0a8527
helenohyeah <ouyang.helen@gmail.com>
2020-11-30 22:30:05 -0500

    Center mute button

--- client/src/components/Canvas/MuteButton.js
+++ client/src/components/Canvas/MuteButton.js
@@ -3,10 +3,10 @@ import React from 'react';
 const MuteButton = (props) => {
 
   return (
-    <aside className="mute-music" >
+    <div className="mute-music" >
       {props.music === "OFF" && <img src="/images/mute-music.png" onClick={() => {props.setMusic("ON")}} alt="unmute music"/>}
       {props.music === "ON" && <img src="/images/unmute-music.png" onClick={() => {props.setMusic("OFF")}} alt="mute music"/>}
-    </aside>
+    </div>
   )
 };
 
